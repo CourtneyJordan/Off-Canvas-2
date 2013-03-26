bfa8df41d5d08bcffb74e76ecd034b6b2b9ed1bc
Courtney Jordan <court.j.jordan@gmail.com>
2013-03-25 20:45:15 -0400

    updated js

--- js/app.js
+++ js/app.js
@@ -1,49 +1,21 @@
-//Create an expandable unordered list with a click event 
-/*$(document).ready(function(){
-	$('.expandable1').on('click', function(event){
-		//alert('hello');
-		event.preventDefault();
-		$('.op-hour-div').toggle();
-	});
-	$('.op-hour-div').on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	}).on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	});
+$(document).ready( function() {
 
-	//Create an expandable unordered list with a click event
-	$('.expandable2').on('click', function(event){
-		//alert('hello');
-		event.preventDefault();
-		$('.parking-div').toggle();
-	});
-	$('.parking-div').on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	}).on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	});
-
-	//Create an expandable unordered list with a click event
-	$('.expandable3').on('click', function(event){
-		//alert('hello');
-		event.preventDefault();
-		$('.public-transit-div').toggle();
-	});
-	$('.public-transit-div').on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	}).on('click', function() {
-		$(this).find('li').slideToggle('slow');
-	});
-});
-*/
-
-$(document).ready(function(){
+	//go out and find anchor(s) that have the class expandable, add click event
 	$('.expandable').on('click', function(event){
+	
+		//prevents default event action on the anchor
 		event.preventDefault();
-		if ($(this).siblings('div').toggle()){
-			$(this).addClass('expanded').show();
-		}else{
-			$(this).removeClass('expanded').hide();			
+		
+		//checks anchor for expanded class
+		if ($(this).hasClass('expanded')){
+		
+			//anchor had class expanded, remove it
+			$(this).removeClass('expanded');
+			
+		} else {
+		
+			//anchor didn't have class expanded, add it
+			$(this).addClass('expanded');			
 		}
 	});
 });
